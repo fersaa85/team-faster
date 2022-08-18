9ee01f74824e32cc61725407fdea4573fad4b15d
MacBook_Air <macbook_air@MacBook-Air-de-MacBook-Air.local>
2022-08-18 12:28:24 -0500

    limit 100

--- app/Rules/UniqueByWorkoutRule.php
+++ app/Rules/UniqueByWorkoutRule.php
@@ -80,7 +80,7 @@ class UniqueByWorkoutRule implements Rule
         }
 
 
-        if(DB::table($this->table)->where('workout_id', $this->workoutId)->count()  > 70){
+        if(DB::table($this->table)->where('workout_id', $this->workoutId)->count()  >= 100){
 
             $this->trans = 'Cupo agotado';
             return false;
