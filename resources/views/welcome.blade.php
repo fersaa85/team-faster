6b86c502747d6068aec741e2a11028c1467b4abe
MacBook_Air <macbook_air@MacBook-Air-de-MacBook-Air.local>
2022-08-19 14:47:45 -0500

    remove victoria

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,6 @@
         <app></app>
         </div>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <script src="{{ mix('js/app.js') }}?v=1.1.4"></script>
+        <script src="{{ mix('js/app.js') }}?v=1.1.5"></script>
     </body>
 </html>
