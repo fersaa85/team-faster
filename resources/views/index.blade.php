2915023d96736ca45ae17fa2005a2cd14e29905f
MacBook_Air <macbook_air@MacBook-Air-de-MacBook-Air.local>
2023-02-14 17:47:12 -0600

    favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="shortcut icon" href="3093logoTeamFaster.ico" type="image/x-icon">
+    <link rel="icon" type="image/png" sizes="16x16"  href="/favicons/favicon-16x16.png">
+    <meta name="msapplication-TileColor" content="#000000">
+    <meta name="theme-color" content="#000000">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}?v=2.0.0" />
     <title>{{env('APP_NAME', 'Team Faster')}}</title>
